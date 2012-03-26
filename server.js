e29bfe0e9bea88bb42fc3125ea6bb6a3afb42005
Andres Rodriguez <andres@joyent.com>
2012-03-26 11:43:00 -0700

    DAPI-2: UFDS and file por module layout in place

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ var path = require('path');
 var fs = require('fs');
 var Logger = require('bunyan');
 
+var interceptors = require('./lib/interceptors');
+var allocation = require('./lib/allocation');
+
+var UFDS = require('./lib/ufds');
 
 var VERSION = false;
 
@@ -50,7 +54,7 @@ function loadConfig() {
 var config = loadConfig();
 
 var log = new Logger({
-  name: 'zapi',
+  name: 'dapi',
   level: config.logLevel,
   serializers: {
     err: Logger.stdSerializers.err,
@@ -60,53 +64,134 @@ var log = new Logger({
 });
 
 
-var server = restify.createServer({
-  name: 'Designation API',
-  log: log,
-  version: version() || '7.0.0',
-  serverName: 'SmartDataCenter',
-  accept: ['text/plain',
-           'application/json',
-           'text/html',
-           'image/png',
-           'text/css'],
-  contentWriters: {
-   'text/plain': function (obj) {
-     if (!obj)
-       return '';
-     if (typeof (obj) === 'string')
-       return obj;
-     return JSON.stringify(obj, null, 2);
-    }
-  }
-});
 
-server.on('uncaughtException', function (req, res, route, error) {
-  req.log.info({
-    err: error,
-    url: req.url,
-    params: req.params
+/*
+ * DAPI constructor
+ */
+function DAPI(options) {
+  this.config = options;
+
+  this.server = restify.createServer({
+    name: 'Designation API',
+    log: log,
+    version: version() || '7.0.0',
+    serverName: 'SmartDataCenter',
+    accept: ['text/plain',
+             'application/json',
+             'text/html',
+             'image/png',
+             'text/css'],
+    contentWriters: {
+     'text/plain': function (obj) {
+       if (!obj)
+         return '';
+       if (typeof (obj) === 'string')
+         return obj;
+       return JSON.stringify(obj, null, 2);
+      }
+    }
   });
 
-  res.send(new restify.InternalError('Internal Server Error'));
-});
+  this.server.on('uncaughtException', function (req, res, route, error) {
+    req.log.info({
+      err: error,
+      url: req.url,
+      params: req.params
+    });
+
+    res.send(new restify.InternalError('Internal Server Error'));
+  });
+}
 
 
-server.get({path: '/', name: 'ListEggs'}, function(req, res, next) {
-  res.send("Hello");
+
+/*
+ * Sets custom middlewares to use for the API
+ */
+DAPI.prototype.setMiddleware = function () {
+  this.server.use(restify.acceptParser(this.server.acceptable));
+  this.server.use(restify.authorizationParser());
+  this.server.use(restify.bodyParser());
+  this.server.use(restify.queryParser());
+}
+
+
+
+/*
+ * Sets all routes for static content
+ */
+DAPI.prototype.setStaticRoutes = function () {
+
+  // TODO: static serve the docs, favicon, etc.
+  //  waiting on https://github.com/mcavage/node-restify/issues/56 for this.
+  this.server.get('/favicon.ico', function (req, res, next) {
+      filed(__dirname + '/docs/media/img/favicon.ico').pipe(res);
+      next();
+  });
+}
+
+
+
+/*
+ * Sets all routes for the ZAPI server
+ */
+DAPI.prototype.setRoutes = function () {
+
+  var before = [
+    addProxies,
+    interceptors.authenticate
+  ];
+
+  allocation.mount(this.server, before);
+}
+
+
+
+/*
+ * Starts listening on the port given specified by config.api.port. Takes a
+ * callback as an argument. The callback is called with no arguments
+ */
+DAPI.prototype.listen = function (callback) {
+  this.server.listen(this.config.api.port, '0.0.0.0', callback);
+}
+
+
+
+/*
+ * Loads UFDS into the request chain
+ */
+function addProxies(req, res, next) {
+  req.config = config;
+  req.ufds = ufds;
+
   return next();
+}
+
+
+var ufds;
+var dapi = new DAPI(config);
+
+try {
+  config.ufds.logLevel = config.logLevel;
+  ufds = new UFDS(config.ufds);
+} catch (e) {
+  console.error('Invalid UFDS config: ' + e.message);
+  process.exit(1);
+}
+
+
+
+ufds.on('ready', function () {
+  dapi.setMiddleware();
+  dapi.setStaticRoutes();
+  dapi.setRoutes();
+
+  dapi.listen(function () {
+    log.info({url: dapi.server.url}, '%s listening', dapi.server.name);
+  });
 });
 
-
-
-// TODO: static serve the docs, favicon, etc.
-//  waiting on https://github.com/mcavage/node-restify/issues/56 for this.
-server.get("/favicon.ico", function (req, res, next) {
-  filed(__dirname + '/docs/media/img/favicon.ico').pipe(res);
-  next();
-});
-
-
-server.listen(config.api.port, function() {
-  console.log('%s listening at %s', server.name, server.url);
+ufds.on('error', function (err) {
+  log.error(err, 'error connecting to UFDS. Aborting.');
+  process.exit(1);
 });
--- test/api.test.js
+++ test/api.test.js
@@ -7,11 +7,10 @@
 var test = require('tap').test;
 
 
-test("douglas adams", function(t) {
-  t.ok(42, "the answer to life the universe and everything");
+test('douglas adams', function (t) {
+  t.ok(42, 'the answer to life the universe and everything');
   t.end();
 });
 
 
-//TODO: fill in a couple tests when have an basic API running
-
+// TODO: fill in a couple tests when have an basic API running
