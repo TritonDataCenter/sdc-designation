3948ebc3043f526daaefce4ef573beb0828791bb
Andres Rodriguez <andres@joyent.com>
2012-03-28 15:55:33 -0700

    DAPI-6: Refactor DAPI object into its own file

--- server.js
+++ server.js
@@ -4,17 +4,10 @@
  * Main entry-point for the Designation API.
  */
 
-var filed = require('filed');
-var restify = require('restify');
-var uuid = require('node-uuid');
 var path = require('path');
 var fs = require('fs');
-var Logger = require('bunyan');
 
-var interceptors = require('./lib/interceptors');
-var allocation = require('./lib/allocation');
-
-var UFDS = require('./lib/ufds');
+var DAPI = require('./lib/dapi')
 
 var VERSION = false;
 
@@ -52,128 +45,14 @@ function loadConfig() {
 }
 
 var config = loadConfig();
+config.version = version() || '7.0.0';
 
-var log = new Logger({
-  name: 'dapi',
-  level: config.logLevel,
-  serializers: {
-    err: Logger.stdSerializers.err,
-    req: Logger.stdSerializers.req,
-    res: restify.bunyan.serializers.response
-  }
-});
-
-
-
-/*
- * DAPI constructor
- */
-function DAPI(options) {
-  this.config = options;
-
-  this.server = restify.createServer({
-    name: 'Designation API',
-    log: log,
-    version: version() || '7.0.0',
-    serverName: 'SmartDataCenter',
-    accept: ['text/plain',
-             'application/json',
-             'text/html',
-             'image/png',
-             'text/css'],
-    contentWriters: {
-     'text/plain': function (obj) {
-       if (!obj)
-         return '';
-       if (typeof (obj) === 'string')
-         return obj;
-       return JSON.stringify(obj, null, 2);
-      }
-    }
-  });
-
-  this.server.on('uncaughtException', function (req, res, route, error) {
-    req.log.info({
-      err: error,
-      url: req.url,
-      params: req.params
-    });
-
-    res.send(new restify.InternalError('Internal Server Error'));
-  });
-}
-
-
-
-/*
- * Sets custom middlewares to use for the API
- */
-DAPI.prototype.setMiddleware = function () {
-  this.server.use(restify.acceptParser(this.server.acceptable));
-  this.server.use(restify.authorizationParser());
-  this.server.use(restify.bodyParser());
-  this.server.use(restify.queryParser());
-}
-
-
-
-/*
- * Sets all routes for static content
- */
-DAPI.prototype.setStaticRoutes = function () {
-
-  // TODO: static serve the docs, favicon, etc.
-  //  waiting on https://github.com/mcavage/node-restify/issues/56 for this.
-  this.server.get('/favicon.ico', function (req, res, next) {
-      filed(__dirname + '/docs/media/img/favicon.ico').pipe(res);
-      next();
-  });
-}
-
-
-
-/*
- * Sets all routes for the ZAPI server
- */
-DAPI.prototype.setRoutes = function () {
-
-  var before = [
-    addProxies,
-    interceptors.authenticate
-  ];
-
-  allocation.mount(this.server, before);
-}
-
-
-
-/*
- * Starts listening on the port given specified by config.api.port. Takes a
- * callback as an argument. The callback is called with no arguments
- */
-DAPI.prototype.listen = function (callback) {
-  this.server.listen(this.config.api.port, '0.0.0.0', callback);
-}
-
-
-
-/*
- * Loads UFDS into the request chain
- */
-function addProxies(req, res, next) {
-  req.config = config;
-  req.ufds = ufds;
-
-  return next();
-}
-
-
-var ufds;
-var dapi = new DAPI(config);
+var dapi;
 
 try {
-  config.ufds.logLevel = config.logLevel;
-  ufds = new UFDS(config.ufds);
+  dapi = new DAPI(config)
+  dapi.init();
+
 } catch (e) {
   console.error('Invalid UFDS config: ' + e.message);
   process.exit(1);
@@ -181,17 +60,11 @@ try {
 
 
 
-ufds.on('ready', function () {
-  dapi.setMiddleware();
-  dapi.setStaticRoutes();
-  dapi.setRoutes();
-
-  dapi.listen(function () {
-    log.info({url: dapi.server.url}, '%s listening', dapi.server.name);
-  });
+dapi.on('ready', function () {
+  dapi.listen();
 });
 
-ufds.on('error', function (err) {
-  log.error(err, 'error connecting to UFDS. Aborting.');
+dapi.on('error', function (err) {
+  dapi.log.error(err, 'error connecting to UFDS. Aborting.');
   process.exit(1);
 });
