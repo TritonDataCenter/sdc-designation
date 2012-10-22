1e9d1496656e94c140c2c1e4cd3c092f11d1ba95
Marsell Kukuljevic <marsell@joyent.com>
2012-10-22 21:33:19 +1100

    DAPI-38 - refactor server.js.

--- server.js
+++ server.js
@@ -4,49 +4,57 @@
  * Main entry-point for the Designation API.
  */
 
-var fs = require('fs');
-
+var fs   = require('fs');
 var DAPI = require('./lib/dapi');
 
-var VERSION = false;
+var CONFIG_PATH  = __dirname + '/config.json';
+var PACKAGE_PATH = __dirname + '/package.json';
 
-/**
+
+
+/*
  * Returns the current semver version stored in CloudAPI's package.json.
  * This is used to set in the API versioning and in the Server header.
- *
- * @return {String} version.
  */
 function version() {
-    if (!VERSION) {
-        var pkg = fs.readFileSync(__dirname + '/package.json', 'utf8');
-        VERSION = JSON.parse(pkg).version;
-    }
+    var pkg = fs.readFileSync(PACKAGE_PATH, 'utf8');
+    version = JSON.parse(pkg).version;
 
-    return VERSION;
+    return version || '7.0.0';
 }
 
 
 
 /*
- * Loads and parse the configuration file at config.json
+ * Loads and parse the configuration file config.json
  */
 function loadConfig() {
-    var configPath = __dirname + '/config.json';
+    try {
+      var configFile = fs.readFileSync(CONFIG_PATH, 'utf-8');
+    } catch (e) {
+        if (e.code !== 'ENOENT')
+            throw (e);
 
-    if (!fs.existsSync(configPath)) {
-        console.error('Config file not found: ' + configPath +
-          ' does not exist. Aborting.');
+        console.error('Config file not found: %s does not exist. Aborting',
+                      CONFIG_PATH);
         process.exit(1);
     }
 
-    var theConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
-    return theConfig;
+    var config = JSON.parse(configFile);
+    return config;
 }
 
-var config = loadConfig();
-config.version = version() || '7.0.0';
 
-var dapi = new DAPI(config);
 
-dapi.init();
-dapi.listen();
+function main() {
+    var config = loadConfig();
+    config.version = version();
+
+    var dapi = new DAPI(config);
+    dapi.init();
+    dapi.listen();
+}
+
+
+
+main();
