6a0000c2abbc68f66f498beffdcf77f60aea4912
Marsell Kukuljevic <marsell@joyent.com>
2012-10-31 03:11:16 +1100

    DAPI-55 - change default plugins in pipline.

--- test/allocation.test.js
+++ test/allocation.test.js
@@ -11,6 +11,47 @@ var common = require('./common');
 
 
 var client;
+var servers = [ {
+    uuid: uuid(),
+    ram: 2048,
+    setup: true,
+    reserved: false,
+    status: 'running',
+    memory_total_bytes: 2147483648,
+    memory_available_bytes: 1073741824,
+    sysinfo: {
+        'Network Interfaces': {
+            e1000g0: {
+                'Link Status': 'up',
+                'NIC Names': [ 'external' ]
+            },
+            e1000g1: {
+               'Link Status': 'up',
+               'NIC Names': [ 'admin' ]
+            }
+        }
+    }
+}, {
+    uuid: uuid(),
+    ram: 1024,
+    setup: true,
+    reserved: false,
+    status: 'running',
+    memory_total_bytes: 1073741824,
+    memory_available_bytes: 536870912,
+    sysinfo: {
+        'Network Interfaces': {
+            e1000g0: {
+                'Link Status': 'up',
+                'NIC Names': [ 'external' ]
+            },
+            e1000g1: {
+               'Link Status': 'up',
+               'NIC Names': [ 'admin' ]
+            }
+        }
+    }
+} ];
 
 
 
@@ -27,59 +68,27 @@ exports.setUp =  function (callback) {
 
 exports.allocation_ok = function (t) {
     var path = '/allocation';
-    var theUuid = uuid();
-    var otherUuid = uuid();
 
-    var servers = [ {
-        uuid: uuid()
-    }, {
-        uuid: otherUuid,
-        ram: 2048,
-        setup: true,
-        memory_total_bytes: 2147483648,
-        memory_available_bytes: 1073741824
-    }, {
-        uuid: theUuid,
-        ram: 1024,
-        setup: true,
-        memory_total_bytes: 1073741824,
-        memory_available_bytes: 536870912
-    } ];
-
-    var data = { servers: servers, vm: { ram: 256 } };
+    var data = { servers: servers,
+                 vm: { ram: 256, nic_tags: [ 'external' ] } };
 
     client.post(path, data, function (err, req, res, body) {
         t.ifError(err);
         t.equal(res.statusCode, 200);
         common.checkHeaders(t, res.headers);
         t.ok(body);
-        t.equal(body.uuid, otherUuid);
+        t.equal(body.uuid, servers[1].uuid);
         t.done();
     });
 };
 
 
 
-exports.allocation_not_ok = function (t) {
+exports.allocation_not_enough_ram = function (t) {
     var path = '/allocation';
-    var theUuid = uuid();
-    var otherUuid = uuid();
 
-    var servers = [ {
-        uuid: uuid()
-    }, {
-        uuid: otherUuid,
-        ram: 2048,
-        memory_total_bytes: 2147483648,
-        memory_available_bytes: 1073741824
-    }, {
-        uuid: theUuid,
-        ram: 1024,
-        memory_total_bytes: 1073741824,
-        memory_available_bytes: 536870912
-    } ];
-
-    var data = { servers: servers, vm: { ram: 2048 } };
+    var data = { servers: servers,
+                 vm: { ram: 2048, nic_tags: [ 'external' ] } };
 
     client.post(path, data, function (err, req, res, body) {
         t.equal(res.statusCode, 409);
