bcd3bd46d033c9c728a50d51c92d0621b31f81ba
Andres Rodriguez <andres@joyent.com>
2013-10-14 20:11:49 -0700

    HEAD-1365 standard log rotation

--- boot/configure.sh
+++ boot/configure.sh
@@ -7,8 +7,6 @@
 export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -o xtrace
 
-PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
-
 echo "Updating SMF manifest"
 $(/opt/local/bin/gsed -i"" -e "s/@@PREFIX@@/\/opt\/smartdc\/dapi/g" /opt/smartdc/dapi/smf/manifests/dapi.xml)
 
--- boot/setup.sh
+++ boot/setup.sh
@@ -10,7 +10,6 @@ set -o xtrace
 PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
 
 role=dapi
-app_name=$role
 
 # Let SAPI know where the local manifests are
 CONFIG_AGENT_LOCAL_MANIFESTS_DIRS=/opt/smartdc/dapi
@@ -29,6 +28,13 @@ chown -R nobody:nobody /opt/smartdc/dapi
 # Add node_modules/bin to PATH
 echo "" >>/root/.profile
 echo "export PATH=\$PATH:/opt/smartdc/$role/build/node/bin:/opt/smartdc/$role/node_modules/.bin" >>/root/.profile
+
+echo "Adding log rotation"
+sdc_log_rotation_add amon-agent /var/svc/log/*amon-agent*.log 1g
+sdc_log_rotation_add config-agent /var/svc/log/*config-agent*.log 1g
+sdc_log_rotation_add registrar /var/svc/log/*registrar*.log 1g
+sdc_log_rotation_add $role /var/svc/log/*$role*.log 1g
+sdc_log_rotation_setup_end
 
 # Install Amon monitor and probes
 TRACE=1 /opt/smartdc/dapi/bin/install-amon-probes
