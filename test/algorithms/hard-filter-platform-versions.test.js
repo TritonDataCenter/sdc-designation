e5c917425584de91a2f841fc8f2df000087332f5
Marsell Kukuljevic <marsell@joyent.com>
2013-10-22 18:02:54 +1100

    DAPI-180 - fix platform reasons to be more intelligible.

--- test/algorithms/hard-filter-platform-versions.test.js
+++ test/algorithms/hard-filter-platform-versions.test.js
@@ -64,10 +64,10 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'b6d9d432-16bd-41b5-b3ac-7e3986380c37': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20121218T203452Z" for version "6.5"',
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20130129T122401Z" for version "6.5"',
-        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "7.0"'
+        'b6d9d432-16bd-41b5-b3ac-7e3986380c37': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20121218T203452Z"}',
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20130129T122401Z"}',
+        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"7.0":"20121210T203034Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -96,10 +96,10 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'b6d9d432-16bd-41b5-b3ac-7e3986380c37': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20121218T203452Z" for version "6.5"',
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires min platform "undefined" for min version "undefined", but server has platform "20130129T122401Z" for version "6.5"',
-        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "7.0"'
+        'b6d9d432-16bd-41b5-b3ac-7e3986380c37': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20121218T203452Z"}',
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"6.5":"20130129T122401Z"}',
+        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires min platforms {"7.0":"20121211T203034Z"}, but server has {"7.0":"20121210T203034Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -132,11 +132,11 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'c15641a8-1dad-4b96-be1e-6aa694395aee': 'Image or package requires max platform "20121211T203034Z" for max version "undefined", but server has platform "20121218T203452Z" for version "7.0"',
-        'c98b17b0-d4f9-4a93-b4da-85ee6a065f8a': 'Image or package requires max platform "20121211T203034Z" for max version "undefined", but server has platform "20130129T122401Z" for version "7.0"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121218T203452Z" for version "7.1"',
-        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121210T203034Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'c15641a8-1dad-4b96-be1e-6aa694395aee': 'Image or package requires max platforms {"7.0":"20121211T203034Z"}, but server has {"7.0":"20121218T203452Z"}',
+        'c98b17b0-d4f9-4a93-b4da-85ee6a065f8a': 'Image or package requires max platforms {"7.0":"20121211T203034Z"}, but server has {"7.0":"20130129T122401Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"7.0":"20121211T203034Z"}, but server has {"7.1":"20121218T203452Z"}',
+        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires max platforms {"7.0":"20121211T203034Z"}, but server has {"7.1":"20121210T203034Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"7.0":"20121211T203034Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -169,14 +169,14 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires max platform "20130128T203034Z" for max version "undefined", but server has platform "20130129T122401Z" for version "6.5"',
-        'c15641a8-1dad-4b96-be1e-6aa694395aee': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121218T203452Z" for version "7.0"',
-        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121210T203034Z" for version "7.0"',
-        'c98b17b0-d4f9-4a93-b4da-85ee6a065f8a': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20130129T122401Z" for version "7.0"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121218T203452Z" for version "7.1"',
-        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20121210T203034Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "undefined" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"6.5":"20130129T122401Z"}',
+        'c15641a8-1dad-4b96-be1e-6aa694395aee': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.0":"20121218T203452Z"}',
+        '9728b8c3-ecbd-4af9-94b0-a1b26e6e5cc0': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.0":"20121210T203034Z"}',
+        'c98b17b0-d4f9-4a93-b4da-85ee6a065f8a': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.0":"20130129T122401Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.1":"20121218T203452Z"}',
+        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.1":"20121210T203034Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"6.5":"20130128T203034Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -211,9 +211,9 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20121218T203452Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20121218T203452Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -249,10 +249,10 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires max platform "20130101T122401Z" for max version "7.1", but server has platform "20130129T122401Z" for version "6.5"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "20121217T203452Z" for max version "7.1", but server has platform "20121218T203452Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "20121217T203452Z" for max version "7.1", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '5d4de22f-e082-43ae-83ec-9957be55f2e1': 'Image or package requires max platforms {"6.5":"20130101T122401Z","7.1":"20121217T203452Z"}, but server has {"6.5":"20130129T122401Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"6.5":"20130101T122401Z","7.1":"20121217T203452Z"}, but server has {"7.1":"20121218T203452Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"6.5":"20130101T122401Z","7.1":"20121217T203452Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -287,9 +287,9 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121211T203034Z" for min version "6.5", but server has platform "20121210T203034Z" for version "6.5"',
-        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires min platform "20121211T203034Z" for min version "6.5", but server has platform "20121210T203034Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "20121219T203452Z" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121211T203034Z","7.1":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        'f1a33640-8657-4572-8061-31e1ecebbade': 'Image or package requires min platforms {"6.5":"20121211T203034Z","7.1":"20121211T203034Z"}, but server has {"7.1":"20121210T203034Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"7.1":"20121219T203452Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -324,9 +324,9 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121218T203452Z" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20121218T203452Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121218T203452Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20121218T203452Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
@@ -363,9 +363,9 @@ function (t) {
 
     var expectedReasons = {
         /* BEGIN JSSTYLED */
-        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platform "20121211T203034Z" for min version "undefined", but server has platform "20121210T203034Z" for version "6.5"',
-        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20121218T203452Z" for version "7.1"',
-        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platform "20121217T203452Z" for max version "undefined", but server has platform "20130129T122401Z" for version "7.1"'
+        'aa652df0-7954-4cbb-9243-3cbb2c99b7be': 'Image or package requires min platforms {"6.5":"20121211T203034Z"}, but server has {"6.5":"20121210T203034Z"}',
+        '9902bee1-fe4a-4f77-93db-951ed5c501bb': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20121218T203452Z"}',
+        '26dbdcdc-ed50-4169-b27f-e12f27c20026': 'Image or package requires max platforms {"7.1":"20121217T203452Z"}, but server has {"7.1":"20130129T122401Z"}'
         /* END JSSTYLED */
     };
     t.deepEqual(reasons, expectedReasons);
