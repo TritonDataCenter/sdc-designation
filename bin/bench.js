ae6d77902a201dbec971d55fe18cbd0b95958919
Marsell Kukuljevic <marsell@joyent.com>
2014-04-29 23:59:05 +1000

    DAPI-221 - update bench script to feed in (new) required data for allocation, and provide a direct driver (instead of just HTTP) by default. The latter is particularly relevant since CNAPI feeds the data in directly, instead of the expensive serialization/deserialization cycle.

--- bin/bench.js
+++ bin/bench.js
@@ -2,16 +2,22 @@
  * Copyright (c) 2012, Joyent, Inc. All rights reserved.
  */
 
+var Allocator = require('../lib/allocator.js');
 var assert = require('assert');
 var Logger = require('bunyan');
 var restify = require('restify');
 var uuid = require('node-uuid');
 
+var MiB = 1024 * 1024;
+var GiB = 1024 * MiB;
 
 
 
-function genServers(numServers, serverRam, numVmsPerServer, vmRam) {
+function genServers(numServers, serverRam, numVmsPerServer) {
+    var reservationRatio = 0.15;
     var numRacks = numServers / 24;
+    var vmRam = serverRam * (1 - reservationRatio - 0.05) / numVmsPerServer;
+
     var servers = [];
 
     for (var i = 0; i !== numServers; i++) {
@@ -20,12 +26,19 @@ function genServers(numServers, serverRam, numVmsPerServer, vmRam) {
             setup: true,
             reserved: false,
             status: 'running',
-            memory_total_bytes: serverRam * 1024 * 1024,
-            memory_available_bytes: (serverRam - 8 * 1024) * 1024 * 1024,
+            memory_total_bytes: serverRam * MiB,
+            memory_available_bytes: serverRam * MiB * reservationRatio,
+            disk_pool_size_bytes: 3600 * GiB,
+            disk_installed_images_used_bytes: 100 * GiB,
+            disk_zone_quota_bytes: 0,
+            disk_kvm_quota_bytes: 0,
+            reservation_ratio: reservationRatio,
             rack_identifier: 'ams-' + i % numRacks,
             sysinfo: {
                 'Zpool Size in GiB': 3600,
-                'CPU Total Cores': 1600,
+                'CPU Total Cores': 24,
+                'SDC Version': '7.0',
+                'Live Image': '20140410T203034Z',
                 'Network Interfaces': {
                     e1000g0: {
                         'Link Status': 'up',
@@ -41,10 +54,13 @@ function genServers(numServers, serverRam, numVmsPerServer, vmRam) {
         };
 
         for (var j = 0; j !== numVmsPerServer; j++) {
+            var cpu = server.sysinfo['CPU Total Cores'];
+
             var vm = {
                 uuid: uuid(),
                 owner_uuid: '98c9f65b-ad6d-4819-bb27-5d00dd54de29',
                 // owner_uuid: uuid(),
+                cpu_cap: 4 * cpu / numVmsPerServer * 100,
                 quota: 15,
                 max_physical_memory: vmRam,
                 zone_state: 'running',
@@ -53,6 +69,8 @@ function genServers(numServers, serverRam, numVmsPerServer, vmRam) {
             };
 
             server.vms[vm.uuid] = vm;
+            server.disk_zone_quota_bytes += 15 * GiB;
+            server.memory_available_bytes -= vmRam * MiB;
         }
 
         servers.push(server);
@@ -63,7 +81,36 @@ function genServers(numServers, serverRam, numVmsPerServer, vmRam) {
 
 
 
-function bench(name, numServers, serverRam, numVmsPerServer, vmRam, callback) {
+function bench(name, driver, numServers, serverRam, numVmsPerServer, callback) {
+    var servers = genServers(numServers, serverRam, numVmsPerServer);
+
+    var data = {
+        servers: servers,
+        vm: {
+            vm_uuid: uuid(),
+            ram: 256,
+            owner_uuid: '98c9f65b-ad6d-4819-bb27-5d00dd54de29',
+            nic_tags: ['external', 'admin']
+        },
+        image: {
+            image_size: 51200,
+            requirements: {
+                min_platform: {
+                    '7.0': '20121218T203452Z'
+                }
+            }
+        },
+        package: {}
+    };
+
+    driver(name, data, callback);
+}
+
+
+
+function http(name, data, callback) {
+    var json = JSON.stringify(data);
+
     var client = restify.createStringClient({
         agent: false,
         url: 'http://localhost:8080',
@@ -73,24 +120,13 @@ function bench(name, numServers, serverRam, numVmsPerServer, vmRam, callback) {
         accept: 'application/json'
     });
 
-    var servers = genServers(numServers, serverRam, numVmsPerServer, vmRam);
-    // var data = { servers: servers, vm: { ram: vmRam, owner_uuid: uuid() } };
-    var data = { servers: servers,
-                 vm: { ram: vmRam,
-                       owner_uuid: '98c9f65b-ad6d-4819-bb27-5d00dd54de29' } };
-    var json = JSON.stringify(data);
-
     var start = new Date();
 
     client.post('/allocation', json, function (err, req, res, body) {
         console.log(name, 'time:', (new Date() - start) / 1000);
 
-//        assert.ifError(err);
-//        assert.equal(res.statusCode, 200);
-//        assert.ok(body);
-
-//        var server = JSON.parse(body);
-//        console.dir(server.uuid);
+        assert.ifError(err);
+        assert.equal(res.statusCode, 200);
 
         if (callback)
             callback();
@@ -99,8 +135,45 @@ function bench(name, numServers, serverRam, numVmsPerServer, vmRam, callback) {
 
 
 
-bench('current', 300, 80 * 1024, 25, 2048, function () {
-    bench('high', 40 * 25, 128 * 1024, 192, 512, function () {
-        bench('ridiculous', 40 * 25, 128 * 1024, 768, 128);
+function direct(name, data, callback) {
+var logStub = { trace: function () { return true; },
+                debug: function () { return true; },
+                info:  function () { return true; },
+                warn:  function () { return true; },
+                error: function (err) { console.log(err); return true; } };
+
+    var allocator = new Allocator(logStub);
+
+    var start = new Date();
+    var results = allocator.allocate(data.servers, data.vm, data.image,
+                                     data.package);
+    assert.ok(results[0]);
+    console.log(name, 'time:', (new Date() - start) / 1000);
+
+    if (callback)
+        callback();
+}
+
+
+
+function main() {
+    var driver;
+
+    if (process.argv[2] === '--http') {
+        console.log('Using HTTP driver');
+        driver = http;
+    } else {
+        console.log('Using direct driver');
+        driver = direct;
+    }
+
+    bench('current', driver, 300, 80 * 1024, 25, function () {
+        bench('high', driver, 40 * 25, 128 * 1024, 192, function () {
+            bench('ridiculous', driver, 40 * 25, 128 * 1024, 768);
+        });
     });
-});
+}
+
+
+
+main();
