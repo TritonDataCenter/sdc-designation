228f9ba2d41696357f23645a36eedf495ee16114
Marsell Kukuljevic <marsell@joyent.com>
2015-08-19 21:27:38 +1000

    CNAPI-551 - allow disabling of override-overprovision plugin, plus allow changing defaults of overprovision ratios for CPU, RAM, disk.

--- test/algorithms/override-overprovisioning.test.js
+++ test/algorithms/override-overprovisioning.test.js
@@ -47,7 +47,7 @@ test('disable overprovisioning', function (t) {
 
 	var state = {};
 	var pkg = {};
-	var constraints = { pkg: pkg };
+	var constraints = { pkg: pkg, defaults: {} };
 
 	var results = filter.run(log, state, givenServers, constraints);
 	var filteredServers = results[0];
@@ -68,7 +68,7 @@ test('disable overprovisioning', function (t) {
 		overprovision_io: 1,
 		overprovision_net: 1
 	};
-	constraints = { pkg: pkg };
+	constraints = { pkg: pkg, defaults: {} };
 
 	filter.run(log, state, givenServers, constraints);
 
@@ -110,8 +110,152 @@ test('disable overprovisioning without pkg', function (t) {
 	];
 
 	var state = {};
+	var constraints = { defaults: {} };
 
-	var results = filter.run(log, state, givenServers, {});
+	var results = filter.run(log, state, givenServers, constraints);
+	var filteredServers = results[0];
+	var reasons = results[1];
+
+	t.deepEqual(filteredServers, expectedServers);
+	t.deepEqual(reasons, undefined);
+
+	t.end();
+});
+
+
+test('disable overprovisioning with disable_override_overprovisioning',
+function (t) {
+	var givenServers = [
+		{ unreserved_cpu: 1 },
+		{ unreserved_cpu: 2, overprovision_ratios: {} },
+		{ unreserved_cpu: 3, overprovision_ratios: { cpu:  1 } },
+		{ unreserved_cpu: 4, overprovision_ratios: { disk: 3 } }
+	];
+
+	// deep copy
+	var expectedServers = JSON.parse(JSON.stringify(givenServers));
+
+	var state = {};
+	var pkg = {};
+	var constraints = { pkg: pkg, defaults: {
+		disable_override_overprovisioning: true
+	} };
+
+	var results = filter.run(log, state, givenServers, constraints);
+	var filteredServers = results[0];
+	var reasons = results[1];
+
+	t.deepEqual(filteredServers, expectedServers);
+	t.deepEqual(pkg, {});
+	t.deepEqual(reasons, undefined);
+
+	t.end();
+});
+
+
+test('disable overprovisioning with override_overprovision_*', function (t) {
+	var givenServers = [
+		{ unreserved_cpu: 1 },
+		{ unreserved_cpu: 2, overprovision_ratios: {} },
+		{ unreserved_cpu: 3, overprovision_ratios: { cpu:  1 } },
+		{ unreserved_cpu: 4, overprovision_ratios: { disk: 3 } }
+	];
+
+	var expectedServers = [
+		{
+			unreserved_cpu: 1,
+			overprovision_ratios: { cpu: 6, ram: 0.75, disk: 0.5 }
+		},
+		{
+			unreserved_cpu: 2,
+			overprovision_ratios: { cpu: 6, ram: 0.75, disk: 0.5 }
+		},
+		{
+			unreserved_cpu: 3,
+			overprovision_ratios: { cpu: 6, ram: 0.75, disk: 0.5 }
+		},
+		{
+			unreserved_cpu: 4,
+			overprovision_ratios: { cpu: 6, ram: 0.75, disk: 0.5 }
+		}
+	];
+
+	var state = {};
+	var pkg = {};
+	var constraints = { pkg: pkg, defaults: {
+		overprovision_ratio_cpu: 6,
+		overprovision_ratio_ram: 0.75,
+		overprovision_ratio_disk: 0.5
+	} };
+
+	var results = filter.run(log, state, givenServers, constraints);
+	var filteredServers = results[0];
+	var reasons = results[1];
+
+	t.deepEqual(filteredServers, expectedServers);
+	t.deepEqual(pkg, {
+		overprovision_cpu: 6,
+		overprovision_ram: 0.75,
+		overprovision_disk: 0.5
+	});
+	t.deepEqual(reasons, undefined);
+
+	pkg = {
+		overprovision_cpu: 1,
+		overprovision_ram: 2,
+		overprovision_disk: 1,
+		overprovision_io: 1,
+		overprovision_net: 1
+	};
+	constraints = { pkg: pkg, defaults: {
+		overprovision_ratio_cpu: 6,
+		overprovision_ratio_ram: 0.75,
+		overprovision_ratio_disk: 0.5
+	} };
+
+	filter.run(log, state, givenServers, constraints);
+
+	t.deepEqual(pkg, {
+		overprovision_cpu: 6,
+		overprovision_ram: 0.75,
+		overprovision_disk: 0.5
+	});
+
+	t.end();
+});
+
+
+test('disable overprovisioning without pkg', function (t) {
+	var givenServers = [
+		{ unreserved_cpu: 1 },
+		{ unreserved_cpu: 2, overprovision_ratios: {} },
+		{ unreserved_cpu: 3, overprovision_ratios: { cpu:  1 } },
+		{ unreserved_cpu: 4, overprovision_ratios: { disk: 3 } }
+	];
+
+	var expectedServers = [
+		{
+			unreserved_cpu: 1,
+			overprovision_ratios: { cpu: 4, ram: 1, disk: 1 }
+		},
+		{
+			unreserved_cpu: 2,
+			overprovision_ratios: { cpu: 4, ram: 1, disk: 1 }
+		},
+		{
+			unreserved_cpu: 3,
+			overprovision_ratios: { cpu: 4, ram: 1, disk: 1 }
+		},
+		{
+			unreserved_cpu: 4,
+			overprovision_ratios: { cpu: 4, ram: 1, disk: 1 }
+		}
+	];
+
+	var state = {};
+	var constraints = { defaults: {} };
+
+	var results = filter.run(log, state, givenServers, constraints);
 	var filteredServers = results[0];
 	var reasons = results[1];
 
@@ -125,7 +269,7 @@ test('disable overprovisioning without pkg', function (t) {
 test('disable overprovisioning with no servers', function (t) {
 	var state = {};
 	var servers = [];
-	var constraints = { pkg: {} };
+	var constraints = { pkg: {}, defaults: {} };
 
 	var results = filter.run(log, state, servers, constraints);
 	var filteredServers = results[0];
